3a5d5dc417d60d15d4c7fffd99c1a3f257db822e
your-username <admin@allied-group.in>
2025-05-04 16:03:03 +0530

    Updated

--- enquiryCapture/outlets/assets/script.js
+++ enquiryCapture/outlets/assets/script.js
@@ -1,5 +1,5 @@
 // Sheet URL ( Publish "Confrigation" Sheet As CSV File And use the CSV Link Here )
-const sourceUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSOd0RZkg-jzsf25xIJXHw1NEW9yyX6CIAXSQZdMmEcUxX3PnKo5dT932d3HoO1OJcpslNvOHNIUreM/pub?gid=326358480&single=true&output=csv';
+const sourceUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5dtpUB9Vutk05o6rsOBRqieHXleMe22yUgVL64jwQVPamDY_JauZ3Z5fmSwp2cItVqg8gsNPdNLrl/pub?gid=1246389069&single=true&output=csv';
 
 
 // Store CSV data globally
@@ -17,38 +17,6 @@ async function fetchCsvData() {
         }
     }
 }
-
-// // Fetch District From Zip Code
-// document.getElementById('zip').addEventListener('input', fetchDistrict);
-// async function fetchDistrict() {
-//     const pinCode = document.getElementById('zip').value;
-//     const districtSelect = document.getElementById('district');
-//     if (pinCode.length === 6 && /^\d{6}$/.test(pinCode)) {
-//         const url = `https://api.postalpincode.in/pincode/${pinCode}`;
-//         try {
-//             const response = await fetch(url);
-//             const data = await response.json();
-//             if (data[0].Status === 'Success') {
-//                 const districts = data[0].PostOffice.map(postOffice => postOffice.District);
-//                 const uniqueDistricts = [...new Set(districts)];
-//                 districtSelect.innerHTML = '';
-//                 uniqueDistricts.forEach(district => {
-//                     const option = document.createElement('option');
-//                     option.value = district;
-//                     option.textContent = district;
-//                     districtSelect.appendChild(option);
-//                 });
-//             } else {
-//                 districtSelect.innerHTML = '<option value="">Invalid zip code</option>';
-//             }
-//         } catch (error) {
-//             districtSelect.innerHTML = '<option value="">Error fetching data</option>';
-//             console.error('Error:', error);
-//         }
-//     } else {
-//         districtSelect.innerHTML = '<option value="">Invalid zip code</option>';
-//     }
-// }
 
 // Enquiry Source Event Handling
 const showroomDropdown = document.getElementById('showroomDropdown');
@@ -102,7 +70,7 @@ function handleEnquirySource(columnIndex, dropdownElement, nameAttribute) {
 // Fetch options for Enquiry Capture By
 async function fetchCaptureByOptions() {
     await fetchCsvData();
-    const captureByOptions = [...new Set(csvData.slice(1).map(row => row[18].trim()).filter(Boolean))];
+    const captureByOptions = [...new Set(csvData.slice(1).map(row => row[6].trim()).filter(Boolean))];
     populateDropdown('captureByDropDown', captureByOptions);
 }
 fetchCaptureByOptions();
@@ -110,7 +78,7 @@ fetchCaptureByOptions();
 // Fetch options for Enquiry Allocated To
 async function fetchAllocatedOptions() {
     await fetchCsvData();
-    const allocatedToOptions = [...new Set(csvData.slice(1).map(row => row[18].trim()).filter(Boolean))];
+    const allocatedToOptions = [...new Set(csvData.slice(1).map(row => row[6].trim()).filter(Boolean))];
     populateDropdown('allocatedToDropDown', allocatedToOptions);
 }
 fetchAllocatedOptions();
@@ -134,7 +102,7 @@ fetchModelColorOptions();
 // Fetch Option For Occupation
 async function fetchOccupationOptions() {
     await fetchCsvData();
-    const occupationOptions = [...new Set(csvData.slice(1).map(row => row[7].trim()).filter(Boolean))];
+    const occupationOptions = [...new Set(csvData.slice(1).map(row => row[9].trim()).filter(Boolean))];
     populateDropdown('Occupation', occupationOptions);
 }
 fetchOccupationOptions();
@@ -163,13 +131,6 @@ document.getElementById("enquiryForm").addEventListener("submit", function (e) {
     e.preventDefault();
     updateTimestamp();
 });
-// function generateUniqueId() {
-//     const now = new Date();
-//     const datePart = now.toLocaleDateString("en-IN", { day: "2-digit", month: "2-digit", year: "2-digit" }).replace(/\//g, "");
-//     const timePart = ("0" + now.getHours()).slice(-2) + ("0" + now.getMinutes()).slice(-2) + ("0" + now.getSeconds()).slice(-2);
-//     document.getElementById("enquiryId").value = "EQ" + datePart + "-" + timePart;
-// }
-// generateUniqueId();
 
 
 // Exchnage Option (Show / Hide)
