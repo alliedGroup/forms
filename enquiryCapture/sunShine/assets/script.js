44cd91151957d02a5d4cf75b1e398731e08cfcfd
your-username <admin@allied-group.in>
2025-07-02 16:01:54 +0530

    Updated

--- enquiryCapture/sunShine/assets/script.js
+++ enquiryCapture/sunShine/assets/script.js
@@ -18,38 +18,6 @@ async function fetchCsvData() {
     }
 }
 
-// // Fetch District From Zip Code
-// document.getElementById('zip').addEventListener('input', fetchDistrict);
-// async function fetchDistrict() {
-//     const pinCode = document.getElementById('zip').value;
-//     const districtSelect = document.getElementById('district');
-//     if (pinCode.length === 6 && /^\d{6}$/.test(pinCode)) {
-//         const url = `https://api.postalpincode.in/pincode/${pinCode}`;
-//         try {
-//             const response = await fetch(url);
-//             const data = await response.json();
-//             if (data[0].Status === 'Success') {
-//                 const districts = data[0].PostOffice.map(postOffice => postOffice.District);
-//                 const uniqueDistricts = [...new Set(districts)];
-//                 districtSelect.innerHTML = '';
-//                 uniqueDistricts.forEach(district => {
-//                     const option = document.createElement('option');
-//                     option.value = district;
-//                     option.textContent = district;
-//                     districtSelect.appendChild(option);
-//                 });
-//             } else {
-//                 districtSelect.innerHTML = '<option value="">Invalid zip code</option>';
-//             }
-//         } catch (error) {
-//             districtSelect.innerHTML = '<option value="">Error fetching data</option>';
-//             console.error('Error:', error);
-//         }
-//     } else {
-//         districtSelect.innerHTML = '<option value="">Invalid zip code</option>';
-//     }
-// }
-
 // Enquiry Source Event Handling
 const showroomDropdown = document.getElementById('showroomDropdown');
 const fieldDropdown = document.getElementById('fieldDropdown');
@@ -163,13 +131,6 @@ document.getElementById("enquiryForm").addEventListener("submit", function (e) {
     e.preventDefault();
     updateTimestamp();
 });
-// function generateUniqueId() {
-//     const now = new Date();
-//     const datePart = now.toLocaleDateString("en-IN", { day: "2-digit", month: "2-digit", year: "2-digit" }).replace(/\//g, "");
-//     const timePart = ("0" + now.getHours()).slice(-2) + ("0" + now.getMinutes()).slice(-2) + ("0" + now.getSeconds()).slice(-2);
-//     document.getElementById("enquiryId").value = "EQ" + datePart + "-" + timePart;
-// }
-// generateUniqueId();
 
 
 // Exchnage Option (Show / Hide)
